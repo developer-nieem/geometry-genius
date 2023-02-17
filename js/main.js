e3396e274945b37521cb6198a9291e2719605452
developer-nieem <naimhossenwork@gmail.com>
2023-02-17 23:09:25 +0600

    create a function for get input value

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 
+function getValueById(inputId){
+    const getInputValueString = document.getElementById(inputId).value;
+    const getInputValue =  parseFloat(getInputValueString);
+    return getInputValue
+}
+
+
 document.getElementById('triangle-btn').addEventListener('click', function(){
     
+    const getInputValueString = document.getElementById('triangle-input-b').value;
+    const getInputValue =  parseFloat(getInputValueString);
+
 })
